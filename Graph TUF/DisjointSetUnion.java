5d0363cdf6b2f79120f345f43c142417a326c71d
Piyush Anand <piyushanand2580@gmail.com>
2024-08-29 14:23:40 +0530

    disjointsetunion by size

--- Graph TUF/DisjointSetUnion.java
+++ Graph TUF/DisjointSetUnion.java
@@ -1,15 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-//Disjoint Set Union by Rank
 class DisjointSet{
     List<Integer> rank = new ArrayList<Integer>();
     List<Integer> parent = new ArrayList<Integer>();
+    List<Integer> size = new ArrayList<Integer>(); //for union by size
     
     public DisjointSet(int n){
         for(int i=0; i<=n; i++){
             rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
 
@@ -23,6 +24,7 @@ class DisjointSet{
         return parent.get(node);
     }
 
+    //Disjoint Set Union by Rank
     public void unionByRank(int u, int v){
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
@@ -39,15 +41,52 @@ class DisjointSet{
             rank.set(ulp_u, rankU+1);
         }
     }
+
+    //Disjoint Set Union by Size
+    public void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        
+        if(size.get(ulp_u) < size.get(ulp_v)){
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v)+size.get(ulp_u));
+        }else{
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
 }
 public class DisjointSetUnion {
     public static void main(String[] args){
         DisjointSet ds = new DisjointSet(7);
-        ds.unionByRank(1, 2);
-        ds.unionByRank(2, 3);
-        ds.unionByRank(4, 5);
-        ds.unionByRank(6, 7);
-        ds.unionByRank(5, 6);
+        // ds.unionByRank(1, 2);
+        // ds.unionByRank(2, 3);
+        // ds.unionByRank(4, 5);
+        // ds.unionByRank(6, 7);
+        // ds.unionByRank(5, 6);
+
+        // //check if 3 and 7 belongs to smae component or not
+        // if(ds.findUPar(3) == ds.findUPar(7)){
+        //     System.out.println("same");
+        // }else{
+        //     System.out.println("not same");
+        // }
+
+        // ds.unionByRank(3, 7);
+        // //now check if 3 and 7 belongs to smae component or not
+        // if(ds.findUPar(3) == ds.findUPar(7)){
+        //     System.out.println("same");
+        // }else{
+        //     System.out.println("not same");
+        // }
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
 
         //check if 3 and 7 belongs to smae component or not
         if(ds.findUPar(3) == ds.findUPar(7)){
@@ -56,7 +95,7 @@ public class DisjointSetUnion {
             System.out.println("not same");
         }
 
-        ds.unionByRank(3, 7);
+        ds.unionBySize(3, 7);
         //now check if 3 and 7 belongs to smae component or not
         if(ds.findUPar(3) == ds.findUPar(7)){
             System.out.println("same");
